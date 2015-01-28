77e8ac0503a7880b358f0c227567e0d402e688f2
infotheme.com <infotheme.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 05:19:16 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/adsense-booster-manager/trunk@1077187 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ global/footer.php
@@ -0,0 +1,8 @@
+<div class="plugin-footer">
+	<div class="plugin-footer-left">
+		<?php _e('Copyright','infotheme');?> <?php _e(date("Y"),'infotheme'); ?> <a href="<?php _e('http://www.infotheme.com','infotheme');?>" ><?php _e('Infotheme','infotheme');?></a>
+	</div>
+	<div class="plugin-footer-right">
+		<?php _e('Proudly powered by','infotheme');?> <a href="<?php _e('http://wordpress.org','infotheme');?>" title="<?php _e('proudly powered by wordpress','infotheme');?>"><?php _e('Wordpress','infotheme');?></a>
+	</div>
+</div>
