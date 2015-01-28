ae78dda2fbcc7cbc89000cdf40b78a3cf55e35f1
infotheme.com <infotheme.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 05:18:21 +0000

    delete
    git-svn-id: https://plugins.svn.wordpress.org/adsense-booster-manager/trunk@1077185 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- global/header.php
+++
@@ -1,9 +0,0 @@
-<div class="plugin-header">
-	<div class="plugin-header-logo">
-		<img src="<?php _e(plugins_url(),'infotheme'); ?>/adsense-booster/images/logo.png" alt="<?php _e('InfoTheme','infotheme');?>" />
-	</div>
-	<div class="plugin-header-right">
-		<h2><?php _e('ADSENSE BOOSTER','infotheme');?></h2>
-		<span class="plugin-version"><?php _e('Ver 1.0','infotheme');?></span>
-	</div>
-</div>
